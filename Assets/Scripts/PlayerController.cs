8cc099eea8d7386d8082daa01403633517516b66
Cory <Coryf65@gmail.com>
2021-08-11 20:24:04 -0500

    Created player basic movement and folllows camera limited by fps

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,9 +9,12 @@ public class PlayerController : MonoBehaviour
     public Transform viewPoint;
     public float mouseSensitivity = 1f;
     public bool IsInverted = false;
+    public float movementSpeed = 5f;
 
     private float verticalRotation;
     private Vector2 mouseInput;
+    private Vector3 moveDirection;
+    private Vector3 movement;
 
 
 
@@ -22,6 +26,26 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        HandlePlayerLookMovement();
+
+        HandlePlayerMovement();
+
+
+
+    }
+
+    private void HandlePlayerMovement()
+    {
+        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+
+        // based on players camera direction, then remove the sideways speed increase
+        movement = ((transform.forward * moveDirection.z) + (transform.right * moveDirection.x)).normalized;
+
+        transform.position += movementSpeed * Time.deltaTime * movement;
+    }
+
+    private void HandlePlayerLookMovement()
     {
         mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
 
@@ -35,12 +59,10 @@ public class PlayerController : MonoBehaviour
         {
             // rotating the view port, clamp to maxs
             viewPoint.rotation = Quaternion.Euler(verticalRotation, viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z);
-        } else
+        }
+        else
         {
             viewPoint.rotation = Quaternion.Euler(-verticalRotation, viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z);
         }
-
-
-
     }
 }
