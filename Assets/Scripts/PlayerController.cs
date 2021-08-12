a9875f7fd71e1d9078180ca8290cab35cd8564c3
Cory <Coryf65@gmail.com>
2021-08-11 19:57:55 -0500

    Added inverting look ability

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,14 +7,17 @@ public class PlayerController : MonoBehaviour
 
     public Transform viewPoint;
     public float mouseSensitivity = 1f;
+    public bool IsInverted = false;
 
     private float verticalRotation;
     private Vector2 mouseInput;
 
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -25,11 +28,18 @@ public class PlayerController : MonoBehaviour
         // rotation to the camera
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
 
-        verticalRotation -= mouseInput.y;
+        verticalRotation += mouseInput.y;
         verticalRotation = Mathf.Clamp(verticalRotation, -60f, 60f);
 
-        // rotating the view port, clamp to maxs
-        viewPoint.rotation = Quaternion.Euler(verticalRotation, viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z);
+        if (IsInverted)
+        {
+            // rotating the view port, clamp to maxs
+            viewPoint.rotation = Quaternion.Euler(verticalRotation, viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z);
+        } else
+        {
+            viewPoint.rotation = Quaternion.Euler(-verticalRotation, viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z);
+        }
+
 
 
     }
