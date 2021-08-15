87bc833d5315668bc43a23fdd738904c22b72091
Cory <Coryf65@gmail.com>
2021-08-15 12:19:59 -0500

    added weapon holders, extracting script into weapons cs

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public bool isFullAuto;
+    // Energy Weapons
+    public float timeBetweenShots = .1f;
+    public float heatPerShot = 1f;
+}
