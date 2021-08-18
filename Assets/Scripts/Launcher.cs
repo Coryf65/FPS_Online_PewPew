87353a4e9af2a69064349906fa7c79b83be5bb3e
Cory <Coryf65@gmail.com>
2021-08-17 20:09:35 -0500

    Able to Leave a room now

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -112,4 +112,18 @@ public class Launcher : MonoBehaviourPunCallbacks
         menuButtons.SetActive(true);
     }
 
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        CloseMenus();
+        loadingText.text = "Leaving room...";
+        loadingScreen.SetActive(true);
+    }
+
+    public override void OnLeftRoom()
+    {
+        CloseMenus();
+        menuButtons.SetActive(true);
+    }
+
 }
