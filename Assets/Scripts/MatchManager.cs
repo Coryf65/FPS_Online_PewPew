0b6427aa7fe4256b104a8667acc3b94b35c024e6
Cory Fabian <Coryf65@gmail.com>
2021-08-20 21:34:24 -0500

    Player hit effects script and prefabs

---
+++ Assets/Scripts/MatchManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+public class MatchManager : MonoBehaviour
+{
+    // singleton
+    public static MatchManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    [Tooltip("Main Menu Scene index on Build Settings.")]
+    public int mainMenu = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PhotonNetwork.IsConnected)
+        {
+            SceneManager.LoadScene(mainMenu);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
