a91b299cc80373546bfe7d9f00226eb7196472f2
Cory Fabian <Coryf65@gmail.com>
2021-09-10 12:54:15 -0500

    Added player selection screen

---
+++ Assets/Scripts/SkinSelector.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkinSelector : MonoBehaviour
+{
+    public Renderer skinImage;
+    public Material[] allSkins;
+    public GameObject playerModel;
+    public Button nextSkinBtn;
+    public Button previousSkinBtn;
+
+    private int skinIndex = 0;
+
+    void Start()
+    {
+        // Set starting Skin selection
+        skinImage.material = allSkins[skinIndex];
+    }
+
+    public void NextSkin()
+    {
+
+        skinIndex++;
+        Debug.Log($"NextSkin, index {skinIndex}");
+
+        if (skinIndex > allSkins.Length - 1)
+        {
+            skinIndex = 0;
+        }
+
+        SetSkin(skinIndex);
+    }
+
+    public void PreviousSkin()
+    {
+        skinIndex--;
+        Debug.Log($"PrevSkin, index {skinIndex}");
+
+        if (skinIndex < 0)
+        {
+            skinIndex = allSkins.Length - 1;
+        }
+
+        SetSkin(skinIndex);
+    }
+
+    void SetSkin(int index)
+    {
+        Debug.Log($"Setting : {skinIndex}");
+        playerModel.GetComponent<Renderer>().material = allSkins[index];
+    }
+}
